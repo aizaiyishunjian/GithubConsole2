10f5e3812a793f3ba6b86432f03bf289eab54743
aizaiyishunjian <1486753326@qq.com>
2016-04-12 22:21:12 +0800

    相对引用路径

--- GithubConsole2/Program.cs
+++ GithubConsole2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,18 @@ namespace GithubConsole2
     {
         static void Main(string[] args)
         {
+            var fileName = "Resource.txt";
+
             Console.WriteLine("Hello Git,I love you!");
 
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            Console.WriteLine("BaseDirectory :{0}",path);
+
+            var filePath = string.Format("{0}\\ResourceFile\\{1}",path,fileName);
+            var file=new FileStream(filePath,FileMode.Open);
+            var reader=new StreamReader(file);
+            
+            Console.WriteLine(reader.ReadLine());
 
             Console.Read();
         }
